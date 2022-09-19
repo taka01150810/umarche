bd5b599adf2a1ceab73e1d31950dc2f7e44622f2
taka01150810 <taka01150810@gmail.com>
2022-09-19 14:15:06 +0900

    マルチログイン(userコントローラ修正)

--- umarche/app/Http/Controllers/User/Auth/RegisteredUserController.php
+++ umarche/app/Http/Controllers/User/Auth/RegisteredUserController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\User\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
@@ -20,7 +20,7 @@ class RegisteredUserController extends Controller
      */
     public function create()
     {
-        return view('auth.register');
+        return view('user.auth.register');
     }
 
     /**
@@ -39,15 +39,15 @@ class RegisteredUserController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        $user = User::create([
+        $users = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
-        event(new Registered($user));
+        event(new Registered($users));
 
-        Auth::login($user);
+        Auth::login($users);
 
         return redirect(RouteServiceProvider::HOME);
     }
