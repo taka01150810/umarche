006026bc629fc3ba7c744bdaa57d97c55c81a273
taka01150810 <taka01150810@gmail.com>
2022-09-20 21:48:00 +0900

    admin(Update更新)

--- umarche/resources/views/admin/owners/edit.blade.php
+++ umarche/resources/views/admin/owners/edit.blade.php
@@ -18,7 +18,9 @@
                         {{-- バリデーションエラー --}}
                         <x-auth-validation-errors class="mb-4" :errors="$errors" />
                         <form method="post" action="{{ route('admin.owners.update', ['owner' => $owner->id ]) }}">
-                            @csrf
+                          {{-- updateはPUTやPATCHで実装 --}}
+                          @method('put')
+                          @csrf
                           <div class="-m-2">
                             <div class="p-2 w-1/2 mx-auto">
                               <div class="relative">
