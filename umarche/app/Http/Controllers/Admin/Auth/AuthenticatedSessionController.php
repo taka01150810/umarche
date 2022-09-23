3ec7a2d77de5840f4da5315ed095183944eb6454
taka01150810 <taka01150810@gmail.com>
2022-09-23 15:11:32 +0900

    エラー対策

--- umarche/app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
+++ umarche/app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
@@ -43,7 +43,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request)
     {
-        Auth::guard('admins')->logout();
+        Auth::guard('admin')->logout();
 
         $request->session()->invalidate();
 
