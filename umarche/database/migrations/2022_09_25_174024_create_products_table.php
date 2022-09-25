950eb2a40cd0c8b3686b9a03c6972ab642e34015
taka01150810 <taka01150810@gmail.com>
2022-09-25 17:58:46 +0900

    owner(Product マイグレーション・シーダー)

--- umarche/database/migrations/2022_09_25_174024_create_products_table.php
+++ umarche/database/migrations/2022_09_25_174024_create_products_table.php
@@ -15,6 +15,15 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('shop_id')//shopが消えたらproductも消える
+            ->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->foreignId('secondary_category_id')//shopが消えてもcategoryは消えない
+            ->constrained();
+            $table->foreignId('image1')
+            ->nullable()
+            ->constrained('images');//image1がカラム名と違うのでテーブル名を指定
             $table->timestamps();
         });
     }
