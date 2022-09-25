0a344967973e189c766326a80c46cbf952efea9a
taka01150810 <taka01150810@gmail.com>
2022-09-25 18:14:50 +0900

    owner(Productリレーション)

--- umarche/app/Models/Product.php
+++ umarche/app/Models/Product.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Shop;
+use App\Models\SecondaryCategory;
+use App\Models\Image;
 
 class Product extends Model
 {
@@ -13,5 +15,18 @@ class Product extends Model
     public function shop()
     {
         return $this->belongsTo(Shop::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(SecondaryCategory::class, 'secondary_category_id');
+    }
+
+    /*
+    メソッド名をモデル名から変える場合は第2引数が必要
+    (カラム名と同じメソッドは指定できないので名称変更)
+    第2引数で  _id   がつかない場合は第3引数で指定が必要
+    */
+    public function imageFirst(){
+        return $this->belongsTo(Image::class, 'image1', 'id');
     }
 }
--- umarche/app/Models/Shop.php
+++ umarche/app/Models/Shop.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use APp\Models\Owner;
+use App\Models\Owner;//スペルミス
 use App\Models\Product;
 
 class Shop extends Model
