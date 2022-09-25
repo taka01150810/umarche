058029ebe7e66d42467777f3473e2a7de42ce11c
taka01150810 <taka01150810@gmail.com>
2022-09-25 11:00:26 +0900

    owner(Image雛形作成)

---
+++ umarche/database/migrations/2022_09_25_105058_create_images_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('owner_id')
+            ->constrained()//外部キー制約
+            ->onUpdate('cascade')//Owner->Shopと外部キー制約を設定しているため追加設定が必要
+            ->onDelete('cascade');
+            $table->string('filename');
+            $table->string('title')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
