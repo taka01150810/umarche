4e364a47bc868c512a78cddb363fecf3e8f5d838
taka01150810 <taka01150810@gmail.com>
2022-09-25 20:13:11 +0900

    owner(Micromodal.js その2)

--- umarche/resources/views/owner/products/create.blade.php
+++ umarche/resources/views/owner/products/create.blade.php
@@ -30,8 +30,9 @@
                                 </div>
                             </div>
 
-                            <x-select-image name="image1"/>
-                            
+                            <div class="p-2 w-1/2 mx-auto">
+                            <x-select-image :images="$images" name="image1"/>
+
                             <div class="p-2 w-full mt-4 flex justify-around">
                                 <button type="button" onclick="location.href='{{ route('owner.products.index') }}'" class="text-white bg-gray-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded text-lg">
                                     戻る
@@ -41,11 +42,29 @@
                                 </button>
                             </div>
 
-                          </div>
+                            </div>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
+    <script>
+    'use strict'
+    const images = document.querySelectorAll('.image') //全てのimageタグを取得
+
+        images.forEach(image => { //1つずつ繰り返す
+            image.addEventListener('click', function(e){ // クリックしたら
+                const imageName = e.target.dataset.id.substr(0, 6) //data-idの6文字
+                const imageId = e.target.dataset.id.replace(imageName + '_', '') // 6文字カット
+                const imageFile = e.target.dataset.file
+                const imagePath = e.target.dataset.path
+                const modal = e.target.dataset.modal
+                // サムネイルと input type=hiddenのvalueに設定
+                document.getElementById(imageName + '_thumbnail').src = imagePath + '/' + imageFile
+                document.getElementById(imageName + '_hidden').value = imageId
+                MicroModal.close(modal); //モーダルを閉じる
+        })
+    })
+    </script>
 </x-app-layout>
