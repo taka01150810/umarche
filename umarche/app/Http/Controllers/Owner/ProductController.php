0971aa0634042e20b33ad46ef59f7460fc266f8d
taka01150810 <taka01150810@gmail.com>
2022-09-25 18:31:51 +0900

    owner(Product Index)

--- umarche/app/Http/Controllers/Owner/ProductController.php
+++ umarche/app/Http/Controllers/Owner/ProductController.php
@@ -4,9 +4,33 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Product;
+use App\Models\Image;
+use App\Models\SecondaryCategory;
+use App\Models\Owner;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function($request, $next){
+
+            $id = $request->route()->parameter('product'); //imageのid取得
+            if(!is_null($id)){ //$idがnullでなかったら
+                $productsOwnerId = Product::findOrFail($id)->shop->owner->id;
+                $productId = (int)$productsOwnerId; // キャスト 文字列→数値に型変換
+                $ownerId = Auth::id();
+
+                if($productId !== $ownerId){ // 同じでなかったら
+                    abort(404); //404画面表示
+                }
+            }
+            return $next($request);
+        });
+    }
     /**
      * Display a listing of the resource.
      *
@@ -15,6 +39,10 @@ class ProductController extends Controller
     public function index()
     {
         //
+        $products = Owner::findOrFail(Auth::id())->shop->product;
+
+        return view('owner.products.index',
+        compact('products'));
     }
 
     /**
