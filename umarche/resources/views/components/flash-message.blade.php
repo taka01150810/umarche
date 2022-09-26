3b39cd11024b2ca8d7d1cf3002c058d961757456
taka01150810 <taka01150810@gmail.com>
2022-09-26 21:40:07 +0900

    エラー対策)

--- umarche/resources/views/components/flash-message.blade.php
+++ umarche/resources/views/components/flash-message.blade.php
@@ -9,7 +9,7 @@ if(session('status') == 'alert'){$bgColor = 'bg-red-500';}
 @endphp
 
 @if(session('message'))
-<div class="{{ $bgColor }} w-1/2 mx-auto p-2 text-white">
+<div class="{{ $bgColor ?? '' }} w-1/2 mx-auto p-2 text-white">
     {{ session('message' )}}
 </div>
 @endif 
