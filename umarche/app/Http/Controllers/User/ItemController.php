deab111b063e49a807271335bc11e07aa89a57b0
taka01150810 <taka01150810@gmail.com>
2022-09-28 19:21:27 +0900

    User(商品一覧ビュー側の調整)

--- umarche/app/Http/Controllers/User/ItemController.php
+++ umarche/app/Http/Controllers/User/ItemController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 // php artisan make:controller User/ItemController で作成
 class ItemController extends Controller
 {
     //
     public function index(){
-        return view('user.index');
+        $products = Product::all();
+        return view('user.index', compact('products'));
     }
 }
