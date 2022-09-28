f21d4e1b226cf18636609e630b29c76a0c37e43d
taka01150810 <taka01150810@gmail.com>
2022-09-28 20:17:26 +0900

    User(商品の詳細 準備)

--- umarche/app/Http/Controllers/User/ItemController.php
+++ umarche/app/Http/Controllers/User/ItemController.php
@@ -11,6 +11,12 @@ use Illuminate\Support\Facades\DB;
 class ItemController extends Controller
 {
     //
+
+    public function __construct()
+    {
+        $this->middleware('auth:users');
+    }
+
     public function index(){
         $stocks = DB::table('t_stocks')
         ->select('product_id',
@@ -47,4 +53,12 @@ class ItemController extends Controller
 
         return view('user.index', compact('products'));
     }
+
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+
+        return view('user.show', compact('product'));
+    }
+
 }
