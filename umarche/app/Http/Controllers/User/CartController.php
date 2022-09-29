1a08a6291d6f454b861c231c0b38d18cf47f22fd
taka01150810 <taka01150810@gmail.com>
2022-09-29 20:13:41 +0900

    User(Cart 保存処理)

--- umarche/app/Http/Controllers/User/CartController.php
+++ umarche/app/Http/Controllers/User/CartController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Cart;
+use App\Models\Cart;//ミスで前回のコミットで追加
+use Illuminate\Support\Facades\Auth;
 
 // php artisan make:controller User/CartControllerで生成
 class CartController extends Controller
@@ -12,6 +13,21 @@ class CartController extends Controller
     //
     public function add(Request $request)
     {
-        dd($request);
+        $itemInCart = Cart::where('user_id', Auth::id())
+        ->where('product_id', $request->product_id)
+        ->first();//カートに商品があるか確認
+        
+        if($itemInCart){
+            $itemInCart->quantity += $request->quantity; //あれば数量を追加
+            $itemInCart->save();
+        } else {
+            Cart::create([ // なければ新規作成
+                'user_id' => Auth::id(),
+                'product_id' => $request->product_id,
+                'quantity' => $request->quantity
+            ]);
+        }
+
+        dd('テスト');
     }
 }
