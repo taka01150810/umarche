cc3ac0f37a6608a9494a29ed8440d124cfe874f2
taka01150810 <taka01150810@gmail.com>
2022-10-01 13:11:54 +0900

    User(User(Stripeキャンセル時の処理))

--- umarche/resources/views/user/checkout.blade.php
+++ umarche/resources/views/user/checkout.blade.php
@@ -9,7 +9,7 @@ window.onload = function() {
     stripe.redirectToCheckout({
         sessionId: '{{ $session->id }}'
     }).then(function (result) {
-        window.location.href = '{{ route('user.cart.index') }}';
+        window.location.href = '{{ route('user.cart.cancel') }}';
     });
 }
 </script>
