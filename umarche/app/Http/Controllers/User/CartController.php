7f685cdba061636702987a4f8cb4578a26406811
taka01150810 <taka01150810@gmail.com>
2022-10-01 12:37:42 +0900

    User(Stripe前の在庫処理)

--- umarche/app/Http/Controllers/User/CartController.php
+++ umarche/app/Http/Controllers/User/CartController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Cart;//ミスで前回のコミットで追加
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Stock;
 
 // php artisan make:controller User/CartControllerで生成
 class CartController extends Controller
@@ -64,16 +65,53 @@ class CartController extends Controller
 
         $lineItems = [];
         foreach($products as $product){
-            $lineItem = [
-                'name' => $product->name,
-                'description' => $product->information,
-                'amount' => $product->price,
-                'currency' => 'jpy',
-                'quantity' => $product->pivot->quantity,
-            ];
-            array_push($lineItems, $lineItem);
+
+            //在庫確認
+            $quantity = '';
+            $quantity = Stock::where('product_id', $product->id)->sum('quantity');
+
+            if($product->pivot->quantity > $quantity ){
+                return redirect()->route('user.cart.index');
+            } else {
+                $lineItem = [
+                    'name' => $product->name,
+                    'description' => $product->information,
+                    'amount' => $product->price,
+                    'currency' => 'jpy',
+                    'quantity' => $product->pivot->quantity,
+                ];
+                array_push($lineItems, $lineItem);
+            }
         }
-        dd($lineItems);
+
+        //決済前に在庫を減らしておく
+        foreach($products as $product){
+            Stock::create([
+                'product_id' => $product->id,
+                'type' => \Constant::PRODUCT_LIST['reduce'],
+                'quantity' => $product->pivot->quantity * -1
+            ]);
+        }
+
+        dd('test');
+
+        \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+        
+        header('Content-Type: application/json');
+        
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],//カード払い
+            'lineItems' => [$lineItems],
+            'mode' => 'payment',//1回払い
+            'success_url' => route('user.items.index'),
+            'cancel_url' => route('user.cart.index'),
+        ]);
+
+        $publicKey = env('STRIPE_PUBLIC_KEY');
+
+        return vieww('user.checkout', 
+        compact('session', 'publicKey'));
+
     }
 
 }
