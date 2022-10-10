f603487e6761bd673641a9252e3b011641dbc301
taka01150810 <taka01150810@gmail.com>
2022-10-10 15:23:28 +0900

    User(オーナー向け商品販売メール)

--- umarche/app/Http/Controllers/User/CartController.php
+++ umarche/app/Http/Controllers/User/CartController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Stock;
 use App\Services\CartService;
+use App\Jobs\SendOrderedMail;
 
 // php artisan make:controller User/CartControllerで生成
 class CartController extends Controller
@@ -62,14 +63,6 @@ class CartController extends Controller
 
     public function checkout()
     {
-        //
-        $items = Cart::where('user_id', Auth::id())->get();
-        $products = CartService::getItemsInCart($items);
-        $user = User::findOrFail(Auth::id());
-
-        SendThanksMail::dispatch($products, $user);
-        dd('ユーザーメール送信テスト');
-
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
 
@@ -126,6 +119,17 @@ class CartController extends Controller
 
     public function success()
     {
+        $items = Cart::where('user_id', Auth::id())->get();
+        $products = CartService::getItemsInCart($items);
+        $user = User::findOrFail(Auth::id());
+
+        SendThanksMail::dispatch($products, $user);
+        foreach($products as $product)
+        {
+            SendOrderedMail::dispatch($product, $user);
+        }
+        // dd('ユーザーメール送信テスト');
+
         Cart::where('user_id', Auth::id())->delete();
         
          return redirect()->route('user.items.index');
