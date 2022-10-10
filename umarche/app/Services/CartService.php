7c95eaddb507d58dcd3c779ac7918fc693f7d3a5
taka01150810 <taka01150810@gmail.com>
2022-10-10 12:50:28 +0900

    User(カート情報から新しく配列をつくる)

--- umarche/app/Services/CartService.php
+++ umarche/app/Services/CartService.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Services;
-
 use App\Models\Product;
 use App\Models\Cart;
 
@@ -11,11 +10,28 @@ class CartService
     {
         $products = []; //空の配列を準備
 
-        dd($items);
-
         foreach($items as $item){ // カート内の商品を一つずつ処理
 
+            $p = Product::findOrFail($item->product_id);
+            $owner = $p->shop->owner->select('name', 'email')->first()->toArray();//オーナー情報
+            $values = array_values($owner); //連想配列の値を取得
+            $keys = ['ownerName', 'email'];
+            $ownerInfo = array_combine($keys, $values); // オーナー情報のキーを変更
+
+            $product = Product::where('id', $item->product_id)
+            ->select('id', 'name', 'price')->get()->toArray(); // 商品情報の配列
+
+            $quantity = Cart::where('product_id', $item->product_id)
+            ->select('quantity')->get()->toArray(); // 在庫数の配列
+
+            // dd($ownerInfo, $product, $quantity);
+
+            $result = array_merge($product[0], $ownerInfo, $quantity[0]); // 配列の結合
+            // dd($result);
+            
+            array_push($products, $result); //配列に追加
         }
+        dd($products);
         return $products; // 新しい配列を返す
     }
 }
